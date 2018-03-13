70808c80433424d06b32e3f2748c853b30847d11
Clément Rigo <clement.rigot@smals.be>
2018-03-13 14:14:19 +0100

    add notification component

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ window.Vue = require('vue');
 
 Vue.component('image-input', require('./components/ImageInput.vue'));
 Vue.component('modal', require('./components/Modal.vue'));
+Vue.component('notification', require('./components/Notification.vue'));
 
 const app = new Vue({
     el: '#vulmadmin'
---
+++ js/components/Notification.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flash notification" :class="[
+        levelClass, isOpen ? isVisibleClass : ''
+    ]">
+        <button class="delete" @click="isOpen = false"></button>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isOpen: false,
+            isVisibleClass: 'is-visible',
+            closeAfter: 10000 // 10 seconds
+        }
+    },
+    props: ['level'],
+    created() {
+        let self = this;
+
+        setTimeout(function() {
+            self.isOpen = true;
+        }, 100);
+
+        setTimeout(function() {
+            self.isOpen = false;
+        }, self.closeAfter);
+    },
+    computed: {
+        levelClass() {
+            return 'is-' + this.level;
+        }
+    },
+}
+</script>
