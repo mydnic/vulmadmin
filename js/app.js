7a8a90d58e19bc5bd7ff057e4248b7661e954505
Clément Rigo <rigoclement@mydnic.be>
2018-03-15 17:28:03 +0100

    try delete button component

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 window.Vue = require('vue');
 
-import {ServerTable, ClientTable, Event} from 'vue-tables-2';
+import { ServerTable, ClientTable, Event } from 'vue-tables-2';
 Vue.use(ClientTable, {}, false, 'bulma');
 
 Vue.component('image-input', require('./components/ImageInput.vue'));
 Vue.component('modal', require('./components/Modal.vue'));
 Vue.component('notification', require('./components/Notification.vue'));
+Vue.component('delete-button', require('./components/DeleteButton.vue'));
 
 const app = new Vue({
     el: '#vulmadmin'
---
+++ js/components/DeleteButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <button class="button is-danger"
+            type="submit"
+            @click="openConfirmationModal">
+            <span class="icon"><i class="fas fa-ban"></i></span>
+            <span>Delete</span>
+        </button>
+        <modal v-if="isModalOpen" @close="isModalOpen = false">
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Please confirm your action</p>
+                    <button class="delete" aria-label="close"></button>
+                </header>
+                <section class="modal-card-body">
+                    Are you sure you want to delete this ?
+                </section>
+                <footer class="modal-card-foot">
+                    <button class="button is-success">Yes, I Confirm</button>
+                    <button class="button">Cancel</button>
+                </footer>
+            </div>
+        </modal>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isModalOpen : false,
+            }
+        },
+        methods: {
+            openConfirmationModal(el) {
+                this.$refs.formConfirm.preventDefault();
+            }
+        }
+    }
+</script>
