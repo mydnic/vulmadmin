de73ace8fcc922bb803ce19975de9053ba5c044c
Clément Rigo <rigoclement@mydnic.be>
2018-04-21 20:40:29 +0200

    remove vue boot to do it from app

--- js/app.js
+++ js/app.js
@@ -7,7 +7,3 @@ Vue.component('image-input', require('./components/ImageInput.vue'));
 Vue.component('modal', require('./components/Modal.vue'));
 Vue.component('notification', require('./components/Notification.vue'));
 Vue.component('delete-button', require('./components/DeleteButton.vue'));
-
-const app = new Vue({
-    el: '#vulmadmin'
-});
